6242a18738a72f314edd49e483fea686a64740f2
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-05-05 05:50:02 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,4 +1,4 @@
 # Write your solution here
 puts "You know what year it is??"
 this_year = Time.now.year
-puts "Hey, it's not 2019" unless this_year == 2019
+puts "Hey, it's 2020" if this_year == 2020
