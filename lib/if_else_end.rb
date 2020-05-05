bc0c9e683fd0613fffb3e69821b5085e7a6fc8e6
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-05-05 05:47:59 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,9 +1,4 @@
 # Write your solution here
-chance_of_rain = 0.3
-if chance_of_rain <= 0.25
-  puts "Pack a sun shelter!"
-elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
-  puts "Pack an umbrella!"
-else
-  puts "Stay home and read Hegel."
-end
+puts "You know what year it is??"
+this_year = 2019
+puts "Hey it's 2019" if this_year == 2019
